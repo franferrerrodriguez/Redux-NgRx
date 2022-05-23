28897f8882c4d36117954a9abc6b6f1eb3b3184b
Francisco José Ferrer Rodríguez <franferrerrodriguez@gmail.com>
2022-05-23 13:51:35 +0200

    05

---
+++ 05_ingresoegresoapp/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngressEgressComponent } from './dashboard/ingress-egress/ingress-egress.component';
+import { DetailComponent } from './dashboard/ingress-egress/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngressEgressComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 05_ingresoegresoapp/src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from "@angular/router";
+import { DetailComponent } from "./ingress-egress/detail/detail.component";
+import { IngressEgressComponent } from "./ingress-egress/ingress-egress.component";
+import { StadisticComponent } from "./ingress-egress/stadistic/stadistic.component";
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: StadisticComponent },
+    { path: 'ingress-egress', component: IngressEgressComponent },
+    { path: 'detail', component: DetailComponent },
+    { path: '**', redirectTo: '' }
+]
